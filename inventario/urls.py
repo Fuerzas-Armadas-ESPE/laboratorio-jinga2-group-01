5dad45018fa6677f49ccbf724bfd30183cb58467
Josué Merino Calderón <josuemerinocalderon11@gmail.com>
2024-06-01 16:02:30 -0500

    Add jinja2 lab

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Crear la URL de la app productos
+    path('', include('productos.urls'))
 ]
